4bd062699149c6650d84cbea0b88ff453119d9b2
repl.it user <replituser@example.com>
2020-10-13 10:13:45 +0000

    Added a robot with dynamic eyes and have started on data types

---
+++ Basics/Input/ascii_robot.py
@@ -0,0 +1,8 @@
+print("Please enter a character for the eyes:")
+eye = input()
+
+print()
+print("##########")
+print("#  "+ eye +"  "+ eye +"  #")
+print("#  ----  #")
+print("##########")
---
+++ Basics/Input/data_types.py
@@ -0,0 +1,11 @@
+name = input("What is your name Human?\n")
+print()
+
+age = input("How old are you? (In years)\n")
+print()
+
+tall = float(input("How tall are you? (In meters)\n"))
+print()
+
+weight = float(input("How much do you weigh? (In kilograms)\n"))
+print()
